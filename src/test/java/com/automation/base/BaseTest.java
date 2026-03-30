0c3f50ee8fe54c6e70a1cd7d657145aec3b3be9e
Luke <luke.walker1234@gmail.com>
2026-03-30 17:01:22 +0100

    feat: add BaseTest with driver setup and teardown

---
+++ src/test/java/com/automation/base/BaseTest.java
@@ -0,0 +1,27 @@
+package com.automation.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromeDriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();    
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
