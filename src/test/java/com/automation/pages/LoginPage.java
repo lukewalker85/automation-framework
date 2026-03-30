b41dff76f4df933895c824d7a5d6ff136a9245de
Luke <luke.walker1234@gmail.com>
2026-03-30 19:05:14 +0100

    feat: add LoginPage with POM and login actions

---
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,37 @@
+package com.automation.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "username")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-btn")
+    private WebElement loginButton;
+
+    @FindBy(id = "error-message")
+    private WebElement errorMessage;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password) {
+        type(usernameField, username);
+        type(passwordField, password);
+        click(loginButton);
+    }
+
+    public String getErrorMessage() {
+        return getText(errorMessage);
+    }
+
+}
