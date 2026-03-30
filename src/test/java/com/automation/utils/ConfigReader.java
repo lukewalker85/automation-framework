fc5de874033ab347b00e135997403bed1bb51dfd
Luke <luke.walker1234@gmail.com>
2026-03-30 21:30:33 +0100

    feat: add ConfigReader utility to load config.properties

---
+++ src/test/java/com/automation/utils/ConfigReader.java
@@ -0,0 +1,26 @@
+package com.automation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+
+public class ConfigReader {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream file = new FileInputStream("src/test/resources/config.properties");
+            properties.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load config.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+    
+}
