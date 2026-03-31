945b176a579d350b1a0b5639ea06e38ed36d5a4c
Luke <luke.walker1234@gmail.com>
2026-03-31 18:11:19 +0100

    feat: add LoginTest and ConfigReader

---
+++ src/test/java/com/automation/tests/LoginTest.java
@@ -0,0 +1,32 @@
+package com.automation.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.automation.base.BaseTest;
+import com.automation.pages.LoginPage;
+import com.automation.utils.ConfigReader;
+
+public class LoginTest extends BaseTest {
+
+    private LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUpPage() {
+        driver.get(ConfigReader.get("BASE_URL"));
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void successfulLoginTest() {
+        loginPage.login(ConfigReader.get("STANDARD_USER"), ConfigReader.get("PASSWORD"));
+        assertThat(driver.getCurrentUrl()).isEqualTo(ConfigReader.get("BASE_URL") + ConfigReader.get("INVENTORY_PATH"));
+    }
+
+    @Test
+    public void invalidLoginTest() {
+        loginPage.login(ConfigReader.get("LOCKED_OUT_USER"), ConfigReader.get("PASSWORD"));
+        assertThat(loginPage.getErrorMessage()).contains("Sorry, this user has been locked out");
+    }
+ }
