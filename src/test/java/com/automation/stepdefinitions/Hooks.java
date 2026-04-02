f3c80a5391c6ce39241d208b6780b812d02d1fef
Luke <luke.walker1234@gmail.com>
2026-04-03 00:31:34 +0100

    feat: add Cucumber Hooks for browser lifecycle management refs #1

---
+++ src/test/java/com/automation/stepdefinitions/Hooks.java
@@ -0,0 +1,25 @@
+package com.automation.stepdefinitions;
+
+import com.automation.base.BaseTest;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    private final BaseTest baseTest;
+
+    public Hooks(BaseTest baseTest) {
+        this.baseTest = baseTest;
+    }
+
+    @Before
+    public void setUp() {
+        baseTest.setUp();
+    }
+
+    @After public void tearDown() {
+        baseTest.tearDown();
+    }
+
+}
