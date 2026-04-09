1cdbfafa46ed284573fda243c09909f694c77533
Luke <luke.walker1234@gmail.com>
2026-04-10 00:54:11 +0100

    feat: parameterise failed login tests with DataProvider closes #2

--- src/test/java/com/automation/tests/LoginTest.java
+++ src/test/java/com/automation/tests/LoginTest.java
@@ -2,16 +2,18 @@ package com.automation.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.automation.base.BaseTest;
 import com.automation.pages.LoginPage;
+import com.automation.testdata.LoginScenario;
 import com.automation.utils.ConfigReader;
 
 public class LoginTest extends BaseTest {
 
     private static ThreadLocal<LoginPage> loginPage = new ThreadLocal<>();
-
+    
     @BeforeMethod
     public void setUpPage() {
         getDriver().get(ConfigReader.get("BASE_URL"));
@@ -24,15 +26,18 @@ public class LoginTest extends BaseTest {
         assertThat(getDriver().getCurrentUrl()).isEqualTo(ConfigReader.get("BASE_URL") + ConfigReader.get("INVENTORY_PATH"));
     }
 
-    @Test
-    public void lockedOutUserTest() {
-        loginPage.get().login(ConfigReader.get("LOCKED_OUT_USER"), ConfigReader.get("PASSWORD"));
-        assertThat(loginPage.get().getErrorMessage()).contains("Sorry, this user has been locked out");
+    @Test(dataProvider="failedLoginData")
+    public void failedLoginTest(LoginScenario scenario) {
+        loginPage.get().login(scenario.username(), scenario.password());
+        assertThat(loginPage.get().getErrorMessage()).contains(scenario.expectedError());
     }
 
-    @Test
-    public void invalidPasswordTest() {
-        loginPage.get().login(ConfigReader.get("STANDARD_USER"), ConfigReader.get("INVALID_PASSWORD"));
-        assertThat(loginPage.get().getErrorMessage()).contains("Username and password do not match any user in this service");
+    @DataProvider(name = "failedLoginData")
+    public Object[][] failedLoginData() {
+        return new Object[][] {
+        {new LoginScenario("locked out user", ConfigReader.get("LOCKED_OUT_USER"), ConfigReader.get("PASSWORD"), "Sorry, this user has been locked out")},
+        {new LoginScenario("invalid password", ConfigReader.get("STANDARD_USER"), ConfigReader.get("INVALID_PASSWORD"), "Username and password do not match any user in this service")}
+        };
     }
+
  }
