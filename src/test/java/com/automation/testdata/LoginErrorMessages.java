2386e0cf28ca107ffacdebc3b0df35f7bb2a3694
Luke <luke.walker1234@gmail.com>
2026-04-12 00:58:03 +0100

    refactor: centralise login error messages in constants class
    Extract duplicated error message strings from LoginSteps and
    LoginTest into a new LoginErrorMessages utility class in the
    testdata package. This removes the risk of test data drift
    when application error text changes.
    
    refs #38

---
+++ src/test/java/com/automation/testdata/LoginErrorMessages.java
@@ -0,0 +1,12 @@
+package com.automation.testdata;
+
+public final class LoginErrorMessages {
+
+    public static final String LOCKED_OUT = "Sorry, this user has been locked out";
+    public static final String INVALID_CREDENTIALS = "Username and password do not match any user in this service";
+
+    private LoginErrorMessages() {
+        // Utility class - prevent instantiation
+    }
+    
+}
