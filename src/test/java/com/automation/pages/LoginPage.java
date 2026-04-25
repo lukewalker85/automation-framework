d14f458b3b12dc3fbef46c1e1c49177a35f7df14
Luke <luke.walker1234@gmail.com>
2026-04-25 18:03:53 +0100

    feat: add SLF4J loggers to framework and page classes refs #51

--- src/test/java/com/automation/pages/LoginPage.java
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -4,8 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Page object for the SauceDemo login page. Provides methods to log in and retrieve error messages.
+ */
 public class LoginPage extends BasePage {
+
+  private static final Logger LOG = LoggerFactory.getLogger(LoginPage.class);
 
   @FindBy(id = "user-name")
   private WebElement usernameField;
@@ -24,12 +31,15 @@ public class LoginPage extends BasePage {
     PageFactory.initElements(driver, this);
   }
 
+  /** Inputs provided username and password and clicks the login button. */
   public void login(String username, String password) {
+    LOG.info("Logging in with username: {}", username);
     type(usernameField, username);
     type(passwordField, password);
     click(loginButton);
   }
 
+  /** Returns error message when login fails. */
   public String getErrorMessage() {
     return getText(errorMessage);
   }
--- src/test/java/com/automation/stepdefinitions/Hooks.java
+++ src/test/java/com/automation/stepdefinitions/Hooks.java
@@ -3,8 +3,17 @@ package com.automation.stepdefinitions;
 import com.automation.base.BaseTest;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Cucumber lifecycle hooks. Manages browser setup and teardown around each scenario by delegating
+ * to BaseTest.
+ */
 public class Hooks {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Hooks.class);
 
   private final BaseTest baseTest;
 
@@ -12,13 +21,17 @@ public class Hooks {
     this.baseTest = baseTest;
   }
 
+  /** Sets up driver before cucumber test */
   @Before
-  public void setUp() {
+  public void setUp(Scenario scenario) {
+    LOG.info("Starting scenario: {}", scenario.getName());
     baseTest.setUp();
   }
 
+  /** Closes down driver once cucumber test has completed */
   @After
-  public void tearDown() {
+  public void tearDown(Scenario scenario) {
     baseTest.tearDown();
+    LOG.info("Finished scenario: {}", scenario.getName());
   }
 }
