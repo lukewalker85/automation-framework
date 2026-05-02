8778281ac287bf21317752d1972cf612d4e18a1d
Luke <luke.walker1234@gmail.com>
2026-05-02 20:15:52 +0100

    refactor: rename LoginRunnerIT to CucumberRunnerIT refs #27

--- src/test/java/com/automation/runners/CucumberRunnerIT.java
+++ src/test/java/com/automation/runners/CucumberRunnerIT.java
@@ -4,12 +4,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
+/** Cucumber TestNG runner for all feature files. */
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "com.automation.stepdefinitions",
     plugin = {"pretty", "html:target/cucumber-reports.html"},
     monochrome = true)
-public class LoginRunnerIT extends AbstractTestNGCucumberTests {
+public class CucumberRunnerIT extends AbstractTestNGCucumberTests {
 
   /** Provides Cucumber scenarios as a TestNG DataProvider with parallel execution enabled. */
   @Override
